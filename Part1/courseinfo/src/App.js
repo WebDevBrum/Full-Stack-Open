878d734b7f39cd8a438a7afb681003ba82646a76
Scott Davies <scottdavies1983@gmail.com>
2021-09-01 06:38:24 +0100

    further part 4 refactor

--- Part1/courseinfo/src/App.js
+++ Part1/courseinfo/src/App.js
@@ -29,26 +29,26 @@ const App = () => {
 
 
 
-  const Content = () => {
+  const Content = (props) => {
     return (
       <>
-        <Part part={parts[0].name} exercise={parts[0].exercises}/>
-        <Part part={parts[1].name} exercise={parts[1].exercises}/>
-        <Part part={parts[2].name} exercise={parts[2].exercises}/>
+        <Part part={props.parts[0].name} exercise={props.parts[0].exercises}/>
+        <Part part={props.parts[1].name} exercise={props.parts[1].exercises}/>
+        <Part part={props.parts[2].name} exercise={props.parts[2].exercises}/>
       </>
     )
   }
   
   const Total = (props) => (
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   )
 
 
   return (
     <div>
       <Header course={course}/>
-      <Content />
-      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
