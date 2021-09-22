b0ff7ddae6dd76e3af0bbfb57455e6780716f86b
Scott Davies <scottdavies1983@gmail.com>
2021-09-22 12:25:23 +0100

    working on single render via button alongside shortlist

--- Part2/countries/src/components/CountryList.js
+++ Part2/countries/src/components/CountryList.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Country from './Country';
 
 const CountryList = ({countries}) => {
+
+  const [visibility, setVisibility] = useState(false);
+  const [countryList, setCountryList] = useState(countries);
+
+  const handleClick = (value) => {
+    setVisibility(!visibility);
+    setCountryList([value]);
+  }
+
+  console.log(countryList);
   return (
     <div>
     {
-      countries.length > 1 
+      countries.length > 1
       ? 
       countries.map((country, index) => 
-        <p key={country.name}>{country.name}</p> 
+      <div key={index} >
+        <p>{country.name}</p>
+        <button  onClick={() => handleClick(country)}>show</button>
+      </div>
+      
         )
         :
-        countries[0] && <Country country={countries[0]}/>
-
+        countries.length > 0  && <Country country={countries[0]}/>
     } 
     </div>
     )
