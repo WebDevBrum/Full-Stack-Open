bc20652dbaf958b99594647c30d958b4377ea7f4
Scott Davies <scottdavies1983@gmail.com>
2021-09-23 09:54:52 +0100

    setup weather api, current server  issue with countries api preventing progress

--- Part2/countries/src/components/Country.js
+++ Part2/countries/src/components/Country.js
@@ -1,7 +1,28 @@
-import React from 'react'
-
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 const Country = ({country}) => {
+
+  const [weather, setWeather] = useState();
+
+  const api_key = process.env.REACT_APP_API_KEY
+  // variable api_key has now the value set in startup
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}`)
+      .then(response => {
+        console.log('promise fulfilled')
+        setWeather(response.data)
+      })
+  }, [country.capital, api_key])
+  
+  console.log('api key', api_key);
+  console.log(weather);
+
+
+
   return (
     <div>
       <h2>{country.name}</h2>
