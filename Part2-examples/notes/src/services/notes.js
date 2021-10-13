1cb58107ecf44fa64ef4c4dd500170ee6be27e8e
Scott Davies <scottdavies1983@gmail.com>
2021-10-13 06:10:45 +0100

    ready for build

--- Part2-examples/notes/src/services/notes.js
+++ Part2-examples/notes/src/services/notes.js
@@ -1,7 +1,7 @@
 
 
 import axios from 'axios'
-const baseUrl = 'https://secure-falls-37285.herokuapp.com/api/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
